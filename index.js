98601b744dd39cdee95b990543704cf11849c7bb
Worriedhenry <kaushikankitsharma05@gmail.com>
2024-11-05 22:25:23 +0530

    Restructured index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Service = require('./serviceSchema');
+const User = require('./userSchema');
+
 const app = express();
 require('dotenv').config();
 app.use(cors());
@@ -21,9 +24,118 @@ app.get("/", (req, res) => {
     res.status(200).send("Connected")
   })
 
-const search=require('./search')
-app.use('/', search)  
+  app.get('/searchname/:query', async (req, res) => {
+    try {
+        const query = req.params.query;
 
-app.listen(5000, () => {
+        const words = query.split(' ');
+
+        const pipeline = [
+            {
+                $match: {
+                    $text: { $search: query } // Search in serviceName
+                }
+            },
+            {
+                $addFields: {
+                    score: {
+                        $size: {
+                            $setIntersection: ['$serviceTags', words] 
+                        }
+                    }
+                }
+            },
+            {
+                $sort: { score: -1 } 
+            },
+            {
+                $limit: 5
+            },
+            {
+                $project: {
+                    _id: 0,
+                    serviceName: 1
+                }
+            }
+        ];
+        const results = await Service.aggregate(pipeline);
+
+        res.json(results);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
+app.get("/search/:query/:page", async (req, res) => {
+    const query = req.params.query;
+    
+    const words = query.split(' ');
+    var page = req.params.page
+    if (!page || page < 1) {
+        page = 1
+    }
+    const pipeline = [
+        {
+            $match: {
+                $text: { $search: query }, 
+            }
+        },
+        {
+            $addFields: {
+                score: {
+                    $size: {
+                        $setIntersection: ['$serviceTags', words] // Calculate the score based on matched tags
+                    }
+                }
+            }
+        },
+        {
+            $sort: { score: -1 } // Sort by score in descending order
+        },
+        {
+            $skip: (page - 1) * 5
+        },
+        {
+            $limit: 5
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "serviceProvider",
+                foreignField: "_id",
+                as: "serviceProvider"
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                serviceName: 1,
+                serviceBrief: 1,
+                serviceTags: 1,
+                serviceCost: 1,
+                serviceCostCurrency: 1,
+                serviceCostDuration: 1,
+                rating: 1,
+                reviewCount: 1,
+                serviceProvider: {
+                    username: "$serviceProvider.username",
+                    profilePic: "$serviceProvider.profilePic",
+                    location: "$serviceProvider.location",
+                    userId: "$serviceProvider._id",
+
+                }
+            }
+        }
+    ];
+
+    const results = await Service.aggregate(pipeline);
+
+
+    res.json(results);
+})
+
+app.listen(5001, () => {
     console.log("Server running on port 5000")
 })  
