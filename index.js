6ad9fb99732b16ef92294c3e1ec31657b6dae9bb
Worriedhenry <kaushikankitsharma05@gmail.com>
2024-11-12 20:22:37 +0530

    Updated Cors

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const User = require('./userSchema');
 
 const app = express();
 require('dotenv').config();
-app.use(cors({
-    origin: '*' ,
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization' 
-
-}));
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB)
